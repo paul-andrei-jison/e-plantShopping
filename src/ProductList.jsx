ccb94b2f582050cd0a4ec9ed1cd194155642fc5e
Paul Jison <paul.andrei.jison@gmail.com>
2025-10-29 02:49:54 -0400

    fixed new bugs

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -279,7 +279,7 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> 1 <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
             </div>
             {!showCart ? (
@@ -289,32 +289,27 @@ function ProductList({ onHomeClick }) {
                             <h3>
                                 <div>{category.category}</div>
                                 <div className='product-list'>
-                                    {category.plants.map((plant) => (
-                                        <div className='product-card'>
+                                {category.plants.map((plant) => {
+                                        const isAdded = cart.find(item => item.name === plant.name);
+
+                                        return (
+                                            <div className='product-card' key={plant.name}>
                                             <img className='product-image' src={plant.image} alt={`${plant.name} image`} />
                                             <div className='product-title'>{plant.name}</div>                                
                                             <div className='product-description'>{plant.description}</div>
                                             <div className='product-cost'>{plant.cost}</div>
                                             <button
-                                            className={`product-button ${cart[plant.name] ? 'disabled' : ''}`}
-                                            onClick={() => handleAddToCart(plant)}
-                                            disabled={cart[plant.name]}
+                                                className={`product-button ${isAdded ? 'disabled' : ''}`}
+                                                onClick={() => handleAddToCart(plant)}
+                                                disabled={isAdded}
                                             >
-                                            {cart[plant.name] ? "Added" : "Add to Cart"}
+                                                {isAdded ? "Added" : "Add to Cart"}
                                             </button>
+                                            </div>
+                                        );
+                                        })}         {isAdded ? "Added" : "Add to Cart"}
+                                            </button>
+                                            </div>
+                                        );
+                                        })}
 
-                                        </div>
-                                    ))}
-                                </div>
-                            </h3>
-                        </div>
-                    ))}
-                </div>
-            ) : (
-                <CartItem onContinueShopping={handleContinueShopping} />
-            )}
-        </div>
-    );
-}
-
-export default ProductList;
